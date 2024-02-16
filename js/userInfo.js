1a125475b6f08426913a321900b3cea602063a52
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-16 11:01:25 +0200

    Secondary commit

---
+++ js/userInfo.js
@@ -0,0 +1,5 @@
+import userName, * as vars from './vars.js';
+
+console.log(`${userName} ${vars.lastName}, have a ${vars.age} years!`);
+
+console.log(vars);
---
+++ js/vars.js
@@ -0,0 +1,6 @@
+let name = 'Roman';
+let age = 23;
+let lastName = 'Chaban';
+
+export default name;
+export { age, lastName };
